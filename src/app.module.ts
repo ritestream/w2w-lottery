c034b04137ad49cfc4743818c05043bcf0a3e1b8
Alex.Xiang <xiangzhe0812@gmail.com>
2024-12-03 10:30:40 +1000

    push update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prismaService';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
